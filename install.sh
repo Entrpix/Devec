5be4e672759e6d8ff234dd7918c74642fe7a6149
Violet :3 <153395462+Entrpix@users.noreply.github.com>
2023-12-27 17:06:28 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,15 +1,10 @@
-echo "Would you like to install the client or the server? (c/s)"
-read opt
+sudo curl "https://entrpix.me/devec/client.sh" -o /usr/local/bin/devec
+sudo chmod +x /usr/local/bin/devec
 
-if [ $opt == "c" ]; then
-    sudo curl "https://entrpix.me/devec/client.sh" -o /usr/local/bin/devec
-    sudo chmod +x /usr/local/bin/devec
-    exit
-elif [ $opt == "s" ]; then
-    sudo curl "https://entrpix.me/devec/server.sh" -o /usr/local/bin/devec-server
-    sudo chmod +x /usr/local/bin/devec-server
-    exit
-else
-    echo "Invalid option"
-    exit
-fi
+sudo curl "https://entrpix.me/devec/server.sh" -o /usr/local/bin/devec-server
+sudo chmod +x /usr/local/bin/devec-server
+
+echo "Client - devec"
+echo "Server - devec-server"
+
+exit
